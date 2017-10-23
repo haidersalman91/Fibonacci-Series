7a421e6f937fce63e2d102907b272f61e7797577
haidersalman91 <32995223+haidersalman91@users.noreply.github.com>
2017-10-23 09:56:29 +0500

    Initial commit

---
+++ Fibonacci Series/Module1.vb
@@ -0,0 +1,34 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim Sum As Integer = 0
+        Dim A As Integer = 0
+        Dim B As Integer = 1
+
+        While True
+
+            A = A + B
+
+            B = A - B
+
+            If A >= 4000000 Then
+                Exit While
+            Else
+                If (A Mod 2 = 0) Then
+
+                    Sum += A
+
+                End If
+            End If
+
+        End While
+
+        MsgBox(" The sum of the even-valued terms is " & Sum & "")
+
+    End Sub
+
+
+   
+
+End Module
